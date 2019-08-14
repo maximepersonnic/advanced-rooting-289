b0e3e757911870853e93e9a1847aec40049f0538
Maxime PERSONNIC <maxime.personnic@hotmail.fr>
2019-08-14 09:38:12 +0200

    add member and collection routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants do
+    collection do                       # collection => no restaurant id in URL
+      get 'top'                       # RestaurantsController#top
+    end
+    member do                             # member => restaurant id in URL
+      get 'chef'                          # RestaurantsController#chef
+    end
+  end
+
 end
